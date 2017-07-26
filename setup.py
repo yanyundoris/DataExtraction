cc4b8eed8f48e627cb9f630b7595dd7cad9b4f60
yanyundoris <yyliuaa@cse.ust.hk>
2017-07-26 13:48:40 +0800

    add
    add

--- setup.py
+++ setup.py
@@ -9,23 +9,42 @@ from setuptools import setup
 import codecs
 import os
 import sys
+import tempfile
+import tarfile
 
-#sys.path.append('/Users/yanyunliu/PycharmProjects/CourseraCrawl/DiscussionForumJava/DataExtraction')
+#sys.path.append('/Users/yanyunliu/PycharmProjects/CourseraCrawl/DataExtraction/')
+# try:
+#     from setuptools import setup
+# except:
+#     from distutils.core import setup
 
-try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
+from distutils.core import setup
+
+
+# Get location of this file at runtime
+global HERE
+HERE = os.path.abspath(os.curdir)
+
+
+print "There is the current path",HERE
+#
+# # Eval the version tuple and string from the source
+# VERSION_NS = {}
+# with open(os.path.join(HERE, 'jupyter_cms', '_version.py')) as f:
+#     exec(f.read(), {}, VERSION_NS)
 
 
 
 def read(fname):
-    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return codecs.open(os.path.join(HERE, fname)).read()
+
+# def read(fname):
+#     return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-NAME = 'DataExtraction'
+NAME = 'ExtractionTool'
 
-#DataExtractionTool", ]
+PACKAGES = ["ExtractionTool"]
 
 DESCRIPTION = "this is a test package."
 
@@ -39,10 +58,10 @@ AUTHOR = "YanyunDoris"
 AUTHOR_EMAIL = "dorisliu9318@gmail.com"
 
 #
-URL = "https://github.com/yanyundoris/eLearning_yyliu/"
+URL = "https://github.com/yanyundoris/DataExtraction"
+DOWNLOAD = 'https://github.com/yanyundoris/DataExtraction/archive/0.1.tar.gz'
 
-
-VERSION = "1.0.3"
+VERSION = "1.0.13"
 
 
 LICENSE = "MIT"
@@ -50,7 +69,7 @@ LICENSE = "MIT"
 
 setup(
     name=NAME,
-    scripts=['DataExtractionTool'],
+    scripts=['DataExtractionTool.py'],
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
@@ -65,7 +84,9 @@ setup(
     author_email=AUTHOR_EMAIL,
     license=LICENSE,
     url= URL,
-    #packages=PACKAGES,
+    packages=PACKAGES,
+    package_dir={'ExtractionTool': 'ExtractionTool'},
     include_package_data=True,
     zip_safe=True,
+    download_url=DOWNLOAD
 )
